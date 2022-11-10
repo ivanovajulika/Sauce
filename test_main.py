5195f2e9029d987971b69f6234ae174cda4238e1
Elena Ladygina <Ladygina_ev@yahoo.com>
2022-11-10 11:37:18 +0700

    added TC_001.00.02 and file configuration by flake8 review by flake8 and black all tests passed

--- test_main.py
+++ test_main.py
@@ -24,7 +24,7 @@ def test_user_can_auth():
     )
 
     browser.get(link)
-    browser.implicitly_wait(1)
+    browser.implicitly_wait(10)
 
     username_input = browser.find_element(By.XPATH, '//*[@id="user-name"]')
     username_input.send_keys(standard_user)
@@ -39,6 +39,8 @@ def test_user_can_auth():
 
 # TC_001.00.02 | Login page > Авторизация при пустом поле "Password"
 def test_password_is_empty():
+    password = ""
+    # browser = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
     options = webdriver.ChromeOptions()
     options.add_argument("--window-size=1600,1080")
     options.headless = True
@@ -47,7 +49,7 @@ def test_password_is_empty():
     )
 
     browser.get(link)
-    browser.implicitly_wait(1)
+    browser.implicitly_wait(10)
 
     username_input = browser.find_element(By.XPATH, '//*[@id="user-name"]')
     username_input.send_keys(standard_user)
@@ -56,7 +58,7 @@ def test_password_is_empty():
     btn_login = browser.find_element(By.XPATH, '//*[@id="login-button"]')
     btn_login.click()
     browser.implicitly_wait(10)
-    error_message = browser.find_element(By.CLASS_NAME, "error-message-container error")
+    error_message = browser.find_element(By.CLASS_NAME, "error-message-container")
     assert (
         error_message.text == "Epic sadface: Password is required"
     ), "Wrong error message"
