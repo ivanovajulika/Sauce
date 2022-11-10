941a03b75dbbcaa4d289be9bf51ffbe9ff7ff54e
Elena Ladygina <Ladygina_ev@yahoo.com>
2022-11-10 11:41:59 +0700

    added TC_001.00.10

--- test_main.py
+++ test_main.py
@@ -63,3 +63,32 @@ def test_password_is_empty():
         error_message.text == "Epic sadface: Password is required"
     ), "Wrong error message"
     browser.quit()
+
+# TC_001.00.10 | Login page > Авторизация заблокированного пользователя
+def test_locked_out_user():
+    options = webdriver.ChromeOptions()
+    options.add_argument("--window-size=1600,1080")
+    options.headless = True
+    browser = webdriver.Chrome(
+        service=ChromeService(ChromeDriverManager().install()), options=options
+    )
+
+    browser.get(link)
+    browser.implicitly_wait(1)
+
+    username_input = browser.find_element(By.XPATH, '//*[@id="user-name"]')
+    username_input.send_keys(locked_out_user)
+    password_input = browser.find_element(By.XPATH, '//*[@id="password"]')
+    password_input.send_keys(password)
+    btn_login = browser.find_element(By.XPATH, '//*[@id="login-button"]')
+    btn_login.click()
+    browser.implicitly_wait(2)
+    assert "inventory" not in browser.current_url, "Wrong page"
+    error = browser.find_element(
+        By.XPATH, '//*[@id="login_button_container"]//div[3]/h3'
+    )
+    assert (
+        error.text == "Epic sadface: Sorry, this user has been locked out."
+    ), "Wrong text"
+
+    browser.quit()
