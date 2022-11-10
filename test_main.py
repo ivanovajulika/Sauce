6e48d04557acd4b6226f985d035ec41467f055aa
Elena Ladygina <Ladygina_ev@yahoo.com>
2022-11-10 11:29:50 +0700

    added TC_001.00.02 and file configuration by flake8

--- test_main.py
+++ test_main.py
@@ -34,4 +34,27 @@ def test_user_can_auth():
     btn_login.click()
     browser.implicitly_wait(10)
     assert "inventory" in browser.current_url, "Wrong page"
-    browser.quit()  # comment
+    browser.quit()
+
+#TC_001.00.02 | Login page > Авторизация при пустом поле "Password"
+def test_password_is_empty():
+    options = webdriver.ChromeOptions()
+    options.add_argument("--window-size=1600,1080")
+    options.headless = True
+    browser = webdriver.Chrome(
+        service=ChromeService(ChromeDriverManager().install()), options=options
+    )
+
+    browser.get(link)
+    browser.implicitly_wait(1)
+
+    username_input = browser.find_element(By.XPATH, '//*[@id="user-name"]')
+    username_input.send_keys(standard_user)
+    password_input = browser.find_element(By.XPATH, '//*[@id="password"]')
+    password_input.send_keys(password)
+    btn_login = browser.find_element(By.XPATH, '//*[@id="login-button"]')
+    btn_login.click()
+    browser.implicitly_wait(10)
+    error_message = browser.find_element(By.CLASS_NAME, 'error-message-container error')
+    assert error_message.text == 'Epic sadface: Password is required', "Wrong error message"
+    browser.quit()
