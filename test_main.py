ea07a501c19360800ac38b2bd44a605cebe2bd6d
QA-BelyaevAA <114562954+QA-BelyaevAA@users.noreply.github.com>
2022-11-10 15:43:51 +0700

    add TC_001.00.04

--- test_main.py
+++ test_main.py
@@ -88,3 +88,31 @@ def test_locked_out_user():
     )
     assert error.text == "Epic sadface: Sorry, this user has been locked out."
     browser.quit()
+
+
+# TC_001.00.04 | Login page > Авторизация при вводе в поле "Password" валидного логина
+def test_password_is_login():
+    password = "standard_user"
+    # browser = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+    options = webdriver.ChromeOptions()
+    options.add_argument("--window-size=1600,1080")
+    options.headless = True
+    browser = webdriver.Chrome(
+        service=ChromeService(ChromeDriverManager().install()), options=options
+    )
+
+    browser.get(link)
+    browser.implicitly_wait(10)
+
+    username_input = browser.find_element(By.XPATH, '//*[@id="user-name"]')
+    username_input.send_keys(standard_user)
+    password_input = browser.find_element(By.XPATH, '//*[@id="password"]')
+    password_input.send_keys(password)
+    btn_login = browser.find_element(By.XPATH, '//*[@id="login-button"]')
+    btn_login.click()
+    browser.implicitly_wait(10)
+    error_message = browser.find_element(By.CLASS_NAME, "error-message-container")
+    assert (
+        error_message.text == "Epic sadface: Username and password do not match any user in this service"
+    ), "Wrong error message"
+    browser.quit()
