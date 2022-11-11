f1d859b00f624a08fb7b8821bdfb5c6b317deeb3
Elena Ladygina <Ladygina_ev@yahoo.com>
2022-11-11 09:00:19 +0700

    Added AT_010.00.01 | Filter > Проверка наличия фильтра на странице "Products" and changed variable password on password_empty in TC_001.00.02

--- test_main.py
+++ test_main.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
 
 # TestData
 link = "https://www.saucedemo.com/"
@@ -37,7 +38,7 @@ def test_user_can_auth():
 
 # TC_001.00.02 | Login page > Авторизация при пустом поле "Password"
 def test_password_is_empty():
-    password = ""
+    password_empty = ""
     # browser = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
     options = webdriver.ChromeOptions()
     options.add_argument("--window-size=1600,1080")
@@ -52,7 +53,7 @@ def test_password_is_empty():
     username_input = browser.find_element(By.XPATH, '//*[@id="user-name"]')
     username_input.send_keys(standard_user)
     password_input = browser.find_element(By.XPATH, '//*[@id="password"]')
-    password_input.send_keys(password)
+    password_input.send_keys(password_empty)
     btn_login = browser.find_element(By.XPATH, '//*[@id="login-button"]')
     btn_login.click()
     browser.implicitly_wait(10)
@@ -88,4 +89,34 @@ def test_locked_out_user():
         By.XPATH, '//*[@id="login_button_container"]//div[3]/h3'
     )
     assert error.text == "Epic sadface: Sorry, this user has been locked out."
+    browser.quit()
+
+
+# AT_010.00.01 | Filter > Проверка наличия фильтра на странице "Products"
+def test_filter_is_present():
+    browser = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+    # options = webdriver.ChromeOptions()
+    # options.add_argument("--window-size=1600,1080")
+    # options.headless = True
+    # browser = webdriver.Chrome(
+    # )
+
+    browser.get(link)
+    browser.implicitly_wait(10)
+
+    username_input = browser.find_element(By.XPATH, '//*[@id="user-name"]')
+    username_input.send_keys(standard_user)
+    password_input = browser.find_element(By.XPATH, '//*[@id="password"]')
+    password_input.send_keys(password)
+    btn_login = browser.find_element(By.XPATH, '//*[@id="login-button"]')
+    btn_login.click()
+    browser.implicitly_wait(10)
+    assert (
+        "inventory" in browser.current_url
+    ), "Wrong page"  # проверяем, что на нужной странице
+    try:
+        browser.find_element(By.CLASS_NAME, "product_sort_container")
+    except NoSuchElementException:
+        assert False
+    assert True
     browser.quit()
