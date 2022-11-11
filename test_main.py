c6f3eba059d18cdf6fa530fbcdf10ccb8a5d1e41
juliadigrigorio <juliadigrigorio@hotmail.com>
2022-11-11 14:55:47 +0300

    TC_001.00.03, TC_001.00.08

--- test_main.py
+++ test_main.py
@@ -121,3 +121,62 @@ def test_filter_is_present():
         assert False
     assert True
     browser.quit()
+
+
+# TC_001.00.03 | Login page > Авторизация при пустом поле "Username"
+def test_username_is_empty():
+    username_empty = ""
+    # browser = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+    options = webdriver.ChromeOptions()
+    options.add_argument("--window-size=1600,1080")
+    options.headless = True
+    browser = webdriver.Chrome(
+        service=ChromeService(ChromeDriverManager().install()), options=options
+    )
+
+    browser.get(link)
+    browser.implicitly_wait(10)
+
+    username_input = browser.find_element(By.XPATH, '//*[@id="user-name"]')
+    username_input.send_keys(username_empty)
+    password_input = browser.find_element(By.XPATH, '//*[@id="password"]')
+    password_input.send_keys(password)
+    btn_login = browser.find_element(By.XPATH, '//*[@id="login-button"]')
+    btn_login.click()
+    browser.implicitly_wait(10)
+    assert "inventory" not in browser.current_url, "Wrong page"
+    error_message = browser.find_element(By.CLASS_NAME, "error-message-container")
+    assert (
+        error_message.text == "Epic sadface: Username is required"
+    ), "Wrong error message"
+    browser.quit()
+
+
+# TC_001.00.08 | Login page > Авторизация при вводе пробелов в поле "Password"
+def test_password_is_whitespace():
+    password_whitespace = "   "
+    # browser = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+    options = webdriver.ChromeOptions()
+    options.add_argument("--window-size=1600,1080")
+    options.headless = True
+    browser = webdriver.Chrome(
+        service=ChromeService(ChromeDriverManager().install()), options=options
+    )
+
+    browser.get(link)
+    browser.implicitly_wait(10)
+
+    username_input = browser.find_element(By.XPATH, '//*[@id="user-name"]')
+    username_input.send_keys(standard_user)
+    password_input = browser.find_element(By.XPATH, '//*[@id="password"]')
+    password_input.send_keys(password_whitespace)
+    btn_login = browser.find_element(By.XPATH, '//*[@id="login-button"]')
+    btn_login.click()
+    browser.implicitly_wait(10)
+    assert "inventory" not in browser.current_url, "Wrong page"
+    error_message = browser.find_element(By.CLASS_NAME, "error-message-container")
+    assert (
+        error_message.text
+        == "Epic sadface: Username and password do not match any user in this service"
+    ), "Wrong error message"
+    browser.quit()
