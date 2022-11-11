d466c8966969d0087330319a1295c638558e0a29
QA-BelyaevAA <114562954+QA-BelyaevAA@users.noreply.github.com>
2022-11-11 16:24:37 +0700

    add TC_001.00.04

--- test_main.py
+++ test_main.py
@@ -113,7 +113,6 @@ def test_password_is_login():
     browser.implicitly_wait(10)
     error_message = browser.find_element(By.CLASS_NAME, "error-message-container")
     assert (
-        error_message.text
-        == "Epic sadface: Username and password do not match any user in this service"
-    ), "Wrong error message"
+        error_message.text == "Epic sadface: Username and password do not match any user in this service"
+    ), " Wrong error message"
     browser.quit()
