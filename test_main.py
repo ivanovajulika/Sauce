3def4866556bca7856260f180b2b0ad9025d6fd9
ivanovajulika <julika77@mail.ru>
2022-11-19 23:55:30 +0700

    TC_002.01.02

--- test_main.py
+++ test_main.py
@@ -12,7 +12,7 @@ from allure_commons.types import AttachmentType
 def test_user_can_go_cart(browser):
     link = "https://www.saucedemo.com/inventory.html"
     page = InventoryPage(browser, link)
-    page.open_page()
+    # page.open_page()
     page.element_cart()
 
 
@@ -33,30 +33,57 @@ def test_user_can_go_continue_shopping(browser):
         )
 
 
-# TC_002.01.01 | Products > Количество товаров на странице "Products"
-@allure.feature("Sauce Labs")
-@allure.story("TC_002.01.01")
-def test_count_products_page(browser):
+@allure.feature("US_002.00 | Products > Страница выбора товаров.")
+@allure.story("TC_002.01.01 | Products > Количество товаров на странице 'Products'")
+def test_count_products(browser):
     link = "https://www.saucedemo.com/inventory.html"
     page = InventoryPage(browser, link)
-    page.open_page()
+    # page.open_page()
     page.count_products()
 
 
-# TC_002.04.01 | Products > Ссылка на страницу товара из карточки товара "Products" - Backpack.
-@allure.feature("Sauce Labs")
-@allure.story("TC_002.04.01")
+@allure.feature("US_002.00 | Products > Страница выбора товаров.")
+@allure.story("TC_002.01.02 | Products > Фото товара на странице 'Products'")
+def test_photo_products(browser):
+    link = "https://www.saucedemo.com/inventory.html"
+    page = InventoryPage(browser, link)
+    # page.open_page()
+    page.should_be_img_backpack()
+    page.should_be_img_bike_light()
+    page.should_be_img_bolt_t_shirt()
+    page.should_be_img_fleece_jacket()
+    page.should_be_img_onesie()
+    page.should_be_img_t_shirt()
+
+
+@allure.feature("US_002.00 | Products > Страница выбора товаров.")
+@allure.story("TC_002.02.01 | Products > Цена товара на странице 'Products' - Backpack")
 @allure.severity("minor")
+def test_link_to_price_backpack(browser):
+    link = "https://www.saucedemo.com/inventory.html"
+    page = InventoryPage(browser, link)
+    # page.open_page()
+    page.should_be_item_backpack()
+    page.price_backpack()
+
+
+@allure.feature("US_002.00 | Products > Страница выбора товаров.")
+@allure.story(
+    "TC_002.04.01 | Products > Ссылка на страницу товара из карточки товара 'Products' - Backpack."
+)
+@allure.severity("critical")
 def test_link_to_inventory_backpack(browser):
     link = "https://www.saucedemo.com/inventory.html"
     page = InventoryPage(browser, link)
-    page.open_page()
+    # page.open_page()
     page.should_be_item_backpack()
     page.item_backpack()
 
 
-# TC_003.00.01 | Inventory item > Переход на страницу товара по клику на картинку товара в его карточке.
-@allure.story("TC_003.00.01")
+@allure.feature("US_003.00 | Inventory item > Страница товара.")
+@allure.story(
+    "TC_003.00.01 | Inventory item > Переход на страницу товара по клику на картинку товара в его карточке."
+)
 def test_link_go_from_img(browser):
     link = "https://www.saucedemo.com/inventory.html"
     page = InventoryPage(browser, link)
@@ -67,12 +94,14 @@ def test_link_go_from_img(browser):
     page.photo_size_required()
 
 
-# TC_003.00.03 | Inventory item > Удаление товара из корзины на странице "Inventory item".
-@allure.story("TC_003.00.03")
+@allure.feature("US_003.00 | Inventory item > Страница товара.")
+@allure.story(
+    "TC_003.00.03 | Inventory item > Удаление товара из корзины на странице 'Inventory item'."
+)
 def test_add_remove_backpack(browser):
     link = "https://www.saucedemo.com/inventory-item.html?id=4"
     page = ItemPage_4(browser, link)
-    page.open_page()
+    # page.open_page()
     page.add_to_cart()
     page.remove_from_cart_btn()
     page.cart_counter(quantity="1")
@@ -86,19 +115,6 @@ def test_add_remove_backpack(browser):
     page.empty_cart()
 
 
-# TC_002.02.01 | Products > Цена товара на странице "Products" - Backpack
-@allure.feature("Sauce Labs")
-@allure.story("TC_002.02.01")
-@allure.severity("minor")
-def test_link_to_price_backpack(browser):
-    link = "https://www.saucedemo.com/inventory.html"
-    page = InventoryPage(browser, link)
-    page.open_page()
-    page.should_be_item_backpack()
-    page.price_backpack()
-
-
-# TC_010.00.01 | Filter > Наличие фильтра на странице "Products"
 @allure.feature("US_010.00 | Filter")
 @allure.story("TC_010.00.01 | Filter > Проверка наличия фильтра на странице 'Products'")
 def test_filter_is_present(browser):
