b89f458aa61059892e30b44711407f6f9f22d053
ivanovajulika <julika77@mail.ru>
2022-11-23 16:27:09 +0700

    TC_004.04.00

--- tests/test_conformily.py
+++ tests/test_conformily.py
@@ -40,13 +40,15 @@ def test_conformity_item(browser):
 
 
 @allure.feature("US_004.00 | Your cart > Страница корзины. Кнопка 'Корзина'.")
-@allure.story("TC_004.02.02 | Your cart > НОВЫЙ")
+@allure.story("TC_ | Your cart > НОВЫЙ")
 def test_conform_all_items(browser):
 
     page = InventoryPage(browser, link)
     page.open_page()
     all_names = list(browser.find_elements(*ALL_NAMES))
     list_all_names = []
+    list_all = []
+    list_all_in_cart = []
     for count, name in enumerate(all_names):
         name = all_names[count].text
         list_all_names.append(name)
@@ -63,7 +65,8 @@ def test_conform_all_items(browser):
         name = all_prices[count].text
         list_all_prices.append(float(name[1:]))
 
-    list_all = zip(list_all_names, list_all_desc, list_all_prices)
+    for i in range(len(list_all_desc)):
+        list_all.append(list((list_all_names[i], list_all_desc[i], list_all_prices[i])))
 
     page.add_all_items()
     time.sleep(5)
@@ -72,24 +75,29 @@ def test_conform_all_items(browser):
     page.open_page()
 
     all_names = list(browser.find_elements(*ALL_NAMES))
-    list_all_names = []
+    list_all_names_cart = []
     for count, name in enumerate(all_names):
         name = all_names[count].text
-        list_all_names.append(name)
+        list_all_names_cart.append(name)
 
     all_desc = list(browser.find_elements(*ALL_DESC))
-    list_all_desc = []
+    list_all_desc_cart = []
     for count, desc in enumerate(all_desc):
         desc = all_desc[count].text
-        list_all_desc.append(desc)
+        list_all_desc_cart.append(desc)
 
     all_prices = list(browser.find_elements(*ALL_PRICES))
-    list_all_prices = []
+    list_all_prices_cart = []
     for count, name in enumerate(all_prices):
         name = all_prices[count].text
-        list_all_prices.append(float(name[1:]))
+        list_all_prices_cart.append(float(name[1:]))
 
-    list_all_in_cart = zip(list_all_names, list_all_desc, list_all_prices)
+    for i in range(len(list_all_desc)):
+        list_all_in_cart.append(
+            list(
+                (list_all_names_cart[i], list_all_desc_cart[i], list_all_prices_cart[i])
+            )
+        )
 
     time.sleep(5)
     assert list_all == list_all_in_cart
