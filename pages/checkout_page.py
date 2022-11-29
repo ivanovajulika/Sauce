79f0bb87d2493a7035d4f0113774e58190e2b125
Elena Ladygina <Ladygina_ev@yahoo.com>
2022-11-29 19:35:26 +0700

    add methods on checkout page

--- pages/checkout_page.py
+++ pages/checkout_page.py
@@ -19,3 +19,18 @@ class Checkout_page(InventoryPage):
     ):  # Негативный тест-кейс - не должно быть перехода на checkout-complete!
         self.browser.find_element(By.CSS_SELECTOR, "#finish").click()
         assert "checkout-complete" not in self.browser.current_url, "Wrong page"
+
+ def btn_cancel_click(self):
+        btn_cancel = self.browser.find_element(*BTN_CANCEL)
+        btn_cancel.click()
+        assert "cart" in self.browser.current_url, "Wrong page"
+
+    def checkout_user_not_click(self, first_name="John", last_name="Smith", zip_postal_code=122):
+        first_name_input = self.browser.find_element(By.CSS_SELECTOR, "#first-name")
+        first_name_input.send_keys(first_name)
+        last_name_input = self.browser.find_element(By.CSS_SELECTOR, "#last-name")
+        last_name_input.send_keys(last_name)
+        zip_postal_code_input = self.browser.find_element(
+            By.CSS_SELECTOR, "#postal-code"
+        )
+        zip_postal_code_input.send_keys(zip_postal_code)
