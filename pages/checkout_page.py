45ed998d19423194cc8398323145f66030509f93
Elena Ladygina <Ladygina_ev@yahoo.com>
2022-11-29 19:36:40 +0700

    added locator btn cancel

--- pages/checkout_page.py
+++ pages/checkout_page.py
@@ -1,6 +1,9 @@
 from selenium.webdriver.common.by import By
 from pages.inventory_page import InventoryPage
 
+# locators
+BTN_CANCEL = (By.ID, "cancel")
+
 
 class Checkout_page(InventoryPage):
     def checkout_user(self, first_name="John", last_name="Smith", zip_postal_code=122):
@@ -20,12 +23,14 @@ class Checkout_page(InventoryPage):
         self.browser.find_element(By.CSS_SELECTOR, "#finish").click()
         assert "checkout-complete" not in self.browser.current_url, "Wrong page"
 
- def btn_cancel_click(self):
+    def btn_cancel_click(self):
         btn_cancel = self.browser.find_element(*BTN_CANCEL)
         btn_cancel.click()
         assert "cart" in self.browser.current_url, "Wrong page"
 
-    def checkout_user_not_click(self, first_name="John", last_name="Smith", zip_postal_code=122):
+    def checkout_user_not_click(
+        self, first_name="John", last_name="Smith", zip_postal_code=122
+    ):
         first_name_input = self.browser.find_element(By.CSS_SELECTOR, "#first-name")
         first_name_input.send_keys(first_name)
         last_name_input = self.browser.find_element(By.CSS_SELECTOR, "#last-name")
