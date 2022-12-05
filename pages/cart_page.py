69a0d6d0170cbeaf57e3553861859701aab0e45f
Elena Ladygina <Ladygina_ev@yahoo.com>
2022-12-05 14:11:42 +0700

    fixed tests from cart page

--- pages/cart_page.py
+++ pages/cart_page.py
@@ -28,7 +28,10 @@ class CartPage(InventoryPage):
     def cart_page_counter(self, quantity):
         """Метод сверяет значение количества товара в колонке QTY с количеством quantity"""
         text = self.browser.find_element(*QTY).text
-        assert int(text) == quantity, f' Wrong quantity, quantity={quantity}, QTY={text}'
+        if quantity >= 0:
+            assert int(text) == quantity, f' Wrong quantity, quantity={quantity}, QTY={text}'
+        else:
+            assert int(text) != quantity, f' Wrong quantity, quantity={quantity}, QTY={text}'
 
     def change_cart_page_counter(self, quantity):
         """Метод отправляет значение quantity в колонку QTY"""
@@ -38,7 +41,7 @@ class CartPage(InventoryPage):
 
     def empty_cart_page(self):
         """Метод проверяет, что значение колонке QTY отсутствует"""
-        assert not self.element_is_present(By.CLASS_NAME, *QTY)
+        assert not self.element_is_present(*QTY)
 
     def list_item_id5(self):
         title_item = self.browser.find_element(
